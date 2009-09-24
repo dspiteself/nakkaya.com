380dc8586a3b5f984454af51551c0762e2b19900
Nurullah Akkaya <nurullah@nakkaya.com>
2009-09-24 15:37:56 +0300

    theme change.

--- app/config.clj
+++ app/config.clj
@@ -1,6 +1,6 @@
 (ns app.config)
 
-(def site-title "Nurullah Akkaya's Home")
+(def site-title "Nurullah Akkaya's blog")
 (def site-url   "http://nakkaya.com")
 (def site-desc  "useless homepage for pointless projects.")
 (def posts-per-page 2)
