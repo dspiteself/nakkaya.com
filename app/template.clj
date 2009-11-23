e2108a6290b8935ec08aa03f38584f4147eb89f6
Nurullah Akkaya <nurullah@nakkaya.com>
2009-11-23 10:57:31 +0200

    favicon added.

--- app/template.clj
+++ app/template.clj
@@ -16,7 +16,8 @@
       {:xmlns "http://www.w3.org/1999/xhtml"}
       [:head
        {:profile "http://gmpg.org/xfn/11"}
-       [:link {:rel "shortcut icon", :href "/favicon.ico"}]
+       [:link {:rel "icon", :href "/images/favicon.ico" :type "image/x-icon"}]
+       [:link {:rel "shortcut icon", :href "/images//favicon.ico" :type "image/x-icon"}]
        [:link
 	{:rel "stylesheet", :type "text/css", :href "/default.css"}]
        [:link
