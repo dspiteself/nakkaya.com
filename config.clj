1f6a8bf86d5da35d89996a2e6e3de6cc055c1892
Nurullah Akkaya <nurullah@nakkaya.com>
2010-08-28 11:19:28 +0300

    switch to static

---
+++ config.clj
@@ -0,0 +1,14 @@
+[:site-title "Nurullah Akkaya: an explorer's log"
+ :site-description "Random bits and pieces on stuff that I find interesting."
+ :site-url "http://nakkaya.com"
+ :in-dir "resources/"
+ :out-dir "site/"
+ :default-template "default.clj"
+ :encoding "UTF-8"
+ :posts-per-page 2
+ :blog-as-index true
+
+ :host "nakkaya.com"
+ :user "nakkaya"
+ :deploy-dir "./www/nakkaya.com/site/"
+ ]
