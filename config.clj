46c23ec1b93329207593800d6e17b135ca1e81f5
Nurullah Akkaya <nurullah@nakkaya.com>
2010-12-27 22:59:55 +0200

    fix emacs path

--- config.clj
+++ config.clj
@@ -7,4 +7,4 @@
  :encoding "UTF-8"
  :posts-per-page 2
  :blog-as-index true
- :emacs "/Users/nakkaya/Dropbox/Applications/Emacs.app/Contents/MacOS/Emacs"]
+ :emacs "/Applications/Emacs.app/Contents/MacOS/Emacs"]
