0db94c84101f4e3384b78b8cf594fa07bc2f8d0e
Nurullah Akkaya <nurullah@nakkaya.com>
2013-01-20 00:38:47 +0200

    update config

--- config.clj
+++ config.clj
@@ -2,17 +2,13 @@
  :site-description "Random bits and pieces on stuff that I find interesting."
  :site-url "http://nakkaya.com"
  :in-dir "resources/"
- ;;:out-dir "/Users/nakkaya/Dropbox/www/nakkaya.com/"
  :out-dir "/home/nakkaya/Dropbox/www/nakkaya.com/"
  :default-template "default.clj"
  :encoding "UTF-8"
  :blog-as-index false
  :create-archives false
- ;;:emacs "/Applications/Emacs.app/Contents/MacOS/Emacs"
- ;;:emacsclient "/Applications/Emacs.app/Contents/MacOS/bin/emacsclient"
- :emacs "/usr/bin/emacs24"
- :emacs-eval ['(add-to-list 'load-path "~/Dropbox/code/emacs/ext/org-mode/lisp/")
-              '(add-to-list 'load-path "~/Dropbox/code/emacs/ext/org-mode/contrib/lisp/")
+ :emacs "/usr/bin/emacs"
+ :emacs-eval ['(add-to-list 'load-path "/usr/share/org-mode/lisp/")
               '(add-to-list 'load-path "~/Dropbox/code/emacs/ext/clojure-mode/")   
               '(require 'htmlize)
               '(require 'org)
